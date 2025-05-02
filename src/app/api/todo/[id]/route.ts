accac8989aca50794acb38d881bfeeda4c21e179
Sebailla <seba.illa.1977@gmail.com>
2025-05-01 21:12:00 -0300

    feat: Next API Routes - RESTful Api Handlers

---
+++ src/app/api/todo/[id]/route.ts
@@ -0,0 +1,83 @@
+import prisma from '@/lib/prisma'
+import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup'
+
+interface Segment {
+    params: {
+        id: string
+    }
+}
+
+export async function GET(request: Request, { params }: Segment) {
+
+    const todoById = await prisma.todo.findFirst({
+        where: {
+            id: params.id
+        }
+    })
+
+    if (!todoById) {
+        return NextResponse.json({
+            status: 'error',
+            error: 'Todo not found',
+            msg: `Todo with id: ${params.id}, not found`
+        }, { status: 404 })
+    }
+
+    return NextResponse.json({
+        status: 'success',
+        todoById
+    })
+}
+
+
+//? Schema de validacion de PATCH
+const updateSchema = yup.object({
+    description: yup.string().optional(),
+    complete: yup.boolean().optional().default(false),
+})
+
+export async function PATCH(request: Request, { params }: Segment) {
+
+    const todoById = await prisma.todo.findFirst({
+        where: {
+            id: params.id
+        }
+    })
+
+    if (!todoById) {
+        return NextResponse.json({
+            status: 'error',
+            error: 'Todo not found',
+            msg: `Todo with id: ${params.id}, not found`
+        }, { status: 404 })
+    }
+
+    try {
+
+        const { complete, description } = await updateSchema.validate(await request.json())
+
+        const updateTodo = await prisma.todo.update({
+            where: {
+                id: params.id
+            },
+            data: {
+                complete,
+                description
+            }
+        })
+
+        return NextResponse.json({
+            status: 'success',
+            updateTodo
+        })
+
+    } catch (error) {
+        return NextResponse.json({
+            status: 'error',
+            error: error.errors
+        }, { status: 400 })
+
+    }
+
+}
---
+++ src/app/api/todo/route.ts
@@ -0,0 +1,69 @@
+import prisma from '@/lib/prisma'
+import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup'
+
+export async function GET(request: Request) {
+
+    const { searchParams } = new URL(request.url)
+    const take = Number(searchParams.get('take') ?? '10')
+    const skip = Number(searchParams.get('skip') ?? '0')
+
+    if (isNaN(take)) {
+        return NextResponse.json({
+            status: 'error',
+            error: 'Invalid take parameter',
+            msg: 'Take must be a number'
+        }, { status: 400 })
+    }
+
+    if (isNaN(skip)) {
+        return NextResponse.json({
+            status: 'error',
+            error: 'Invalid skip parameter',
+            msg: 'Skip must be a number'
+        }, { status: 400 })
+    }
+
+    const todos = await prisma.todo.findMany({
+        take: take,
+        skip: skip,
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+
+    return NextResponse.json({
+        status: 'success',
+        todos
+    })
+}
+
+
+//? Schema de validacion de POST
+const postSchema = yup.object({
+    description: yup.string().required(),
+    complete: yup.boolean().optional().default(false),
+})
+
+export async function POST(request: Request) {
+
+    try {
+        const { description, complete } = await postSchema.validate(await request.json())
+
+        const newTodo = await prisma.todo.create({
+            data: {description, complete}
+        })
+
+        return NextResponse.json({
+            status: 'success',
+            newTodo
+        })
+
+    } catch (error) {
+        return NextResponse.json({
+            status: 'error',
+            msg: error.message
+        }, { status: 400 })
+    }
+
+}
